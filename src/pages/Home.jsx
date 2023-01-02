303195ee53e3241e957d4bd564d65544b12782ac
viviannemelo <viviannemmelo@gmail.com>
2023-01-02 15:59:56 -0300

    Animation done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Curriculo from '../components/doc/curriculo-vivianne-melo.pdf';
 
+
 class Home extends Component {
   render() {
     return (
       <div id="Home" className='Home'>
         <div className='info-main'>
           <span>Olá, visitante! :)</span>
-          <h1>Vivianne Melo</h1>
+          <h1 id="typewriter">Vivianne Melo</h1>
           <h2>Desenvolvedora Web Front-end Junior
           </h2>
           <a href={Curriculo} download="Currículo - Vivianne Melo">
